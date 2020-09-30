b94bf0f385b5567e22cbeb34aa938d901656b720
Your Name <bod_0e0@hotmail.com>
2020-09-30 09:03:35 +0800

    php-MVC

---
+++ core/App.php
@@ -0,0 +1,36 @@
+<?php
+
+class App {
+    
+    public function __construct() {
+        $url = $this->parseUrl();
+        
+        $controllerName = "{$url[0]}Controller";
+        
+        $url = $this->parseUrl();
+        
+        $controllerName = "{$url[0]}Controller";
+        if (!file_exists("controllers/$controllerName.php"))
+            return;
+        require_once "controllers/$controllerName.php";
+        $controller = new $controllerName;
+        $methodName = isset($url[1]) ? $url[1] : "index";
+        if (!method_exists($controller, $methodName))
+            return;
+        unset($url[0]); unset($url[1]);
+        $params = $url ? array_values($url) : Array();
+        call_user_func_array(Array($controller, $methodName), $params);
+   
+    }
+    
+    public function parseUrl() {
+        if (isset($_GET["url"])) {
+            $url = rtrim($_GET["url"], "/"); //rtrim從字符串右侧移除字符：
+            $url = explode("/", $url); //字串切割函數,並存為陣列
+            return $url;
+        }
+    }
+    
+}
+
+?>
